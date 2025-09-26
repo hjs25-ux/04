b725669e715befbd51fde43c9a8c850538c28a73
hjs25-ux <hjs25@sookmyung.ac.kr>
2025-09-26 20:14:33 +0900

    실습5

--- main.c
+++ main.c
@@ -1,29 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[])
-{
-   int time, sec, minute, hour;
-   
-   printf("Input seconds:");
-   scanf("%i", sec);
-   
-   hour = time/3600;
-   minute = (time%3600)/60;
-   sec = time/60;
-   
-   printf("The time is %i:%i:%i", hour,minute,sec);
-   
-   system("PAUSE");
-   return 0;
-    int year;
-
+int main(int argc, char *argv[]){
+    unsigned int x;
+    int b;
     
-    printf("Input year:");
-    scanf("%i", &year);
+    printf("Input a number:");
+    scanf("%ui",&x);
+    
+    for (b=0; x!=0; x >>= 1)
+    {
+        if (x & 1)
+        {
+              b++;
+        }
+    }
 
-        
-    printf("Is the year %i leap year? : %i\n", year, ((year%4==0)&&(year%100!=0)) || (year%400 ==0));
+    printf("The result is:%i\n", b);
+    
     system("PAUSE");	
     return 0;
 }
